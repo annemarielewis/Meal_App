1201aaf4bc24af1f61d020f69311f0a8be4e157d
Josh Hutchison <joshhutchison@hotmail.com>
2023-11-01 01:39:21 -0400

    commented out bootstrap import from CountryList

--- Recipe_Search/src/components/countries/CountryList.jsx
+++ Recipe_Search/src/components/countries/CountryList.jsx
@@ -2,7 +2,7 @@
 import axios from "axios"; //package
 import { useState, useEffect } from "react"; //react library
 import { Link } from "react-router-dom"; //React Router library
-import "bootstrap";
+// import "bootstrap";
 import React from "react";
 
 let response;
