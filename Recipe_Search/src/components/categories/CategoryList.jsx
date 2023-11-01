137af66bc97bebff7265bc47ef2b99d06a3bbf0c
darcystruble <darcyhallstruble@gmail.com>
2023-10-31 20:27:36 -0400

    category components prepped

--- Recipe_Search/src/components/categories/CategoryList.jsx
+++ Recipe_Search/src/components/categories/CategoryList.jsx
@@ -1,8 +1,43 @@
-export default function CategoryList() {
+import { Link } from 'react-router-dom'
+import { Spinner, Card, CardBody, CardTitle, CardText, Button } from 'reactstrap'
 
-    return(
-        <>
-            
-        </>
-    )
+// Category > Meal in Category > Individual meal
+// https://www.themealdb.com/api/json/v1/1/categories.php
+// https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef
+// https://www.themealdb.com/api/json/v1/1/lookup.php?i=52874
+
+export default function CategoryList(props) {
+    // loading when the cards are loading
+    if (props.caterogies.length === 0) {
+        return (
+            <Spinner className="m-5" color="primary">
+                Loading...
+            </Spinner>
+        )
+    } else {
+        return(
+            <div>
+                {/* MAP IN HERE, PUT CARD IN MAP */}
+                <Card style={{ width: '18rem' }} >
+                    <img alt='src = .strCaterogryThumb' src="https://picsum.photos/300/200" />
+                    <CardBody>
+                    <CardTitle tag="h5">
+                        title = .strCategroy
+                    </CardTitle>
+                    <CardText>
+                        text = .strCatergoryDescription
+                    </CardText>
+                    <Button>
+                        <Link></Link> Go to category meals
+                    </Button>
+                </CardBody>
+                </Card>
+                <div>
+                    <Button color="primary">
+                        Button back to home
+                    </Button>
+                    </div>
+            </div>
+        )
+    }
 }
--- Recipe_Search/src/components/categories/CategoryMeals.jsx
+++ Recipe_Search/src/components/categories/CategoryMeals.jsx
@@ -1,8 +1,37 @@
-export default function CategoryMeals() {
+import { Link, useNaivate } from 'react-router-dom'
+import { Spinner, Card, CardBody, CardTitle, Button } from 'reactstrap'
 
-    return(
-        <>
-            
-        </>
-    )
+export default function CategoryMeals(props) {
+    // navigate to individual meal
+    // api link uses individual id number found in meals list (idMeal)
+    // https://www.themealdb.com/api/json/v1/1/lookup.php?i=52874
+    let navigate = useNaivate()
+    const showMeal = (meal) => {
+        navigate(`${meal.name}`)
+    }
+    // if none, show loading sign
+    if (props.catergory.length === 0){
+        return (
+            <Spinner className="m-5" color="primary">
+                Loading...
+            </Spinner>
+        )
+    } else {
+        return(
+            <div>
+                {/* MAP IN HERE, PUT CARD INSIDE MAP */}
+                <Card style={{ width: '18rem' }} key={idMeal}>
+                    <img alt='src = .strMealThumb' src="https://picsum.photos/300/200" />
+                    <CardBody>
+                    <CardTitle tag="h5">
+                        title = .strMeal
+                    </CardTitle>
+                    <Button>
+                        <Link></Link> Go to category meals
+                    </Button>
+                </CardBody>
+                </Card>
+            </div>
+        )
+    }
 }
