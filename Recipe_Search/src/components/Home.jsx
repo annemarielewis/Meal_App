f9d522bc36a2c4f1cf36413dc19462d22fcf1d0e
darcystruble <darcyhallstruble@gmail.com>
2023-11-01 16:51:27 -0400

    updates

--- Recipe_Search/src/components/Home.jsx
+++ Recipe_Search/src/components/Home.jsx
@@ -1,10 +1,22 @@
 import RandomDish from "./RandomDish"
+import SmallMeals from "./SmallMeals"
+import Search from "./Search"
 
 export default function Home() {
 
     return(
-        <>
-            <RandomDish></RandomDish>
-        </>
+        <div className="home-outer">
+            <RandomDish/>
+            <div className="seperate">
+                <div className="search-message">Find your next favorite meal.</div>
+                <Search />
+            </div>
+            <div className="small-outer">
+                <SmallMeals />
+                <SmallMeals />
+                <SmallMeals />
+            </div>
+
+        </div>
     )
 }
---
+++ Recipe_Search/src/components/SmallMeals.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import axios from "axios"
+
+export default function SmallMeals () {
+    const [randomRecipe, setRandomRecipe] = useState('')
+
+    useEffect(()=> {
+        const getRecipe = async () => {
+            const response = await axios.get(`https://www.themealdb.com/api/json/v1/1/random.php`)
+            // console.log(response)
+            setRandomRecipe(response.data.meals[0])
+        }
+        getRecipe()
+    },[])
+    console.log(randomRecipe)
+    return (
+        <Link to={`/category/meal/${randomRecipe.idMeal}`} className="small-outer">
+            <div>
+               <img src={randomRecipe.strMealThumb} className="small-img"/>
+               <h2 className="small-head">{randomRecipe.strMeal}</h2>
+            </div>
+        </Link>
+    )
+}
